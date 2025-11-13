7499f4cf280c4239cfbd87849addf22932ef7a99
Clayton Drown <Crd@drownfamily.com>
2025-11-13 18:08:34 -0500

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,20 @@
-// eslint.config.js
 import js from "@eslint/js";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-	{
-		plugins: {
-			js,
-		},
-		extends: ["js/recommended"],
-		rules: {
-			"no-unused-vars": "warn",
-		},
-	},
+  {
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      env: {
+        browser: true,   // adds document, window, console, fetch, etc.
+        es2021: true,    // modern JS globals
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
 ]);
